2802062a838484d484c3922600892455c93a52db
alifseptiandii <164166617+septiandyy@users.noreply.github.com>
2024-07-04 19:42:53 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,119 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "In which country Taj Mahal Situated?",
+    answer: "India",
+    options: [
+      "Pakistan",
+      "Bhutan",
+      "Thailand",
+      "India"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which planet is the 3rd closest to the sun?",
+    answer: "Earth",
+    options: [
+      "Jupiter",
+      "Earth",
+      "Mercury",
+      "Venus"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What art form is described as 'decorative handwriting or handwritten lettering'?",
+    answer: "Caligraphy",
+    options: [
+      "Caligraphy",
+      "Alphabet",
+      "Latin",
+      "Roman"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many players can be on a football field at once?",
+    answer: "22",
+    options: [
+      "11",
+      "21",
+      "12",
+      "22"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "What does the Roman numeral XC represent?",
+    answer: "90",
+    options: [
+      "2100",
+      "90",
+      "1100",
+      "3100"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Which of the following is NOT a Greek god or goddess?",
+    answer: "Divinity",
+    options: [
+      "Oceanus",
+      "Hypnos",
+      "Divinity",
+      "Chaos"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Which country exports the most coffee?",
+    answer: "Brazil",
+    options: [
+      "Indonesia",
+      "Brazil",
+      "Germany",
+      "Sweden"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "What is the heaviest organ in the human body?",
+    answer: "Skin",
+    options: [
+      "Brain",
+      "Heart",
+      "Skin",
+      "Liver"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "What were the dates of WWII?",
+    answer: "1939-1945",
+    options: [
+      "1940-1945",
+      "1942-1945",
+      "1939-1944",
+      "1939-1945"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "What is the largest desert in the world?",
+    answer: "Antarctica",
+    options: [
+      "Sahara",
+      "Namib",
+      "Antarctica",
+      "Arctic"
+    ]
+  },
+];
